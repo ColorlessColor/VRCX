9f558436ddd18a632495c0e4d1a677df3cbc1db6
Misaka-L <lipww1234@foxmail.com>
2026-02-05 17:32:05 +0800

    refactor: better ipc

---
+++ src-dotnet/Core/VRCX.Core/Models/Ipc/IpcPacketPayload.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace VRCX.Core.Models.Ipc;
+
+public record IpcPacketPayload(
+    [property: JsonPropertyName("type")] string Type
+);
+
+public record LaunchCommandIpcPacketPayload(
+    [property: JsonPropertyName("command")]
+    string Command
+) : IpcPacketPayload("LaunchCommand");
+
+public record IpcOutPacketPayload(
+    string Type,
+    string? Data,
+    string? MsgType
+);
