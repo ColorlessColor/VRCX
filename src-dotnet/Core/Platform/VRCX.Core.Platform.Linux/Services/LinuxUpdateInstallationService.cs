af481bb61d103fbfa7b6adc7a2342382484c97c0
ColorlessColor <171957320+ColorlessColor@users.noreply.github.com>
2026-02-09 20:20:40 +0800

    feat: implement LinuxUpdateInstallationService

--- src-dotnet/Core/Platform/VRCX.Core.Platform.Linux/Services/LinuxUpdateInstallationService.cs
+++ src-dotnet/Core/Platform/VRCX.Core.Platform.Linux/Services/LinuxUpdateInstallationService.cs
@@ -9,28 +9,67 @@ public sealed class LinuxUpdateInstallationService(
 ) : IUpdateInstallationService
 {
     private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+    private static string InstallerPath => Path.Join(AppPathService.AppDataDirectory, "VRCX_Setup");
+
     public ValueTask<bool> IsInstallerReadyAsync()
     {
-        throw new NotImplementedException();
+        return ValueTask.FromResult(File.Exists(InstallerPath));
     }
 
     public ValueTask PrepareUpdateInstallationAsync(string pathToInstaller)
     {
-        throw new NotImplementedException();
+        if (File.Exists(InstallerPath))
+        {
+            _logger.Info("Deleting existing installer at {InstallerPath}", InstallerPath);
+            File.Delete(InstallerPath);
+        }
+
+        File.Move(pathToInstaller, InstallerPath);
+        return ValueTask.CompletedTask;
     }
 
-    public ValueTask InstallUpdateAsync()
+    public async ValueTask InstallUpdateAsync()
     {
-        throw new NotImplementedException();
+        if (!File.Exists(InstallerPath))
+        {
+            throw new FileNotFoundException("Installer not found", InstallerPath);
+        }
+
+        var vrcxProcess = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = InstallerPath,
+                UseShellExecute = true,
+                WorkingDirectory = AppPathService.AppDataDirectory
+            }
+        };
+
+        vrcxProcess.Start();
+
+        await platformLifetimeService.InvokeShutdownAsync();
     }
 
     public ValueTask CleanupAfterInstallationAsync()
     {
-        throw new NotImplementedException();
+        if (File.Exists(InstallerPath))
+        {
+            _logger.Info("Cleaning up installer at {InstallerPath}", InstallerPath);
+            File.Delete(InstallerPath);
+        }
+
+        return ValueTask.CompletedTask;
     }
 
     public ValueTask CancelUpdateInstallationAsync()
     {
-        throw new NotImplementedException();
+        if (File.Exists(InstallerPath))
+        {
+            _logger.Info("Cancelling update installation and deleting installer at {InstallerPath}", InstallerPath);
+            File.Delete(InstallerPath);
+        }
+
+        return ValueTask.CompletedTask;
     }
 }
