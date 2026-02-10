20eb1b1bacc8efdf751b60f8409dfb08dffec70b
Misaka-L <lipww1234@foxmail.com>
2026-02-10 17:57:41 +0800

    feat: wait for web loaded timeout

--- src-dotnet/App/VRCX.App/Services/NotifyWebLoadedService.cs
+++ src-dotnet/App/VRCX.App/Services/NotifyWebLoadedService.cs
@@ -1,13 +1,30 @@
-﻿using VRCX.Core.Services.Platform;
+﻿using NLog;
+using VRCX.Core.Services.Platform;
 
 namespace VRCX.App.Services;
 
 public sealed class NotifyWebLoadedService : INotifyWebLoadedService
 {
+    private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
     private readonly TaskCompletionSource _webLoadedTcs = new();
+    private readonly CancellationTokenSource _cts = new();
+
+    public NotifyWebLoadedService()
+    {
+        _cts.Token.Register(() =>
+        {
+            if (_webLoadedTcs.Task.IsCompleted)
+                return;
+
+            _logger.Warn("WaitForWebLoadedAsync timed out, setting result anyway.");
+            _webLoadedTcs.TrySetResult();
+        });
+    }
 
     public Task WaitForWebLoadedAsync()
     {
+        _cts.CancelAfter(TimeSpan.FromSeconds(5));
         return _webLoadedTcs.Task;
     }
 
