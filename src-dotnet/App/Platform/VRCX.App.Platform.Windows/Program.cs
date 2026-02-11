c32697982715a8ac647f2b53fcd11c5a4ca51755
Misaka-L <lipww1234@foxmail.com>
2026-02-11 14:54:05 +0800

    refactor: remove inter fonts

--- src-dotnet/App/Platform/VRCX.App.Platform.Windows/Program.cs
+++ src-dotnet/App/Platform/VRCX.App.Platform.Windows/Program.cs
@@ -27,6 +27,5 @@ class Program
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
-            .WithInterFont()
             .LogToTrace();
 }
