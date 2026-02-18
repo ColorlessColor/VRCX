fc3f1abf37dc6ff028f3b57788c1d6e49816fa19
ColorlessColor <171957320+ColorlessColor@users.noreply.github.com>
2026-02-18 20:22:12 +0800

    feat: implement LinuxUpdateInstallationService, WIP

--- src-dotnet/Core/Platform/VRCX.Core.Platform.Linux/Services/LinuxUpdateInstallationService.cs
+++ src-dotnet/Core/Platform/VRCX.Core.Platform.Linux/Services/LinuxUpdateInstallationService.cs
@@ -11,53 +11,65 @@ public sealed class LinuxUpdateInstallationService(
 {
     private readonly ILogger _logger = Log.ForContext<LinuxUpdateInstallationService>();
 
-    private static string InstallerPath => Path.Join(AppPathService.AppDataDirectory, "VRCX_Setup");
+    private static string AppImagePathOld => Path.Join(AppPathService.AppDataDirectory, "VRCX.AppImage.old");
+
+    private static string AppImagePathNew => Path.Join(AppPathService.AppDataDirectory, "VRCX.AppImage.new");
 
     public ValueTask<bool> IsInstallerReadyAsync()
     {
-        return ValueTask.FromResult(File.Exists(InstallerPath));
+        return ValueTask.FromResult(File.Exists(AppImagePathNew));
     }
 
     public ValueTask PrepareUpdateInstallationAsync(string pathToInstaller)
     {
-        if (File.Exists(InstallerPath))
+        if (File.Exists(AppImagePathNew))
         {
-            _logger.Information("Deleting existing installer at {InstallerPath}", InstallerPath);
-            File.Delete(InstallerPath);
+            _logger.Information("Deleting existing installer at {AppImagePathNew}", AppImagePathNew);
+            File.Delete(AppImagePathNew);
         }
 
-        File.Move(pathToInstaller, InstallerPath);
+        File.Move(pathToInstaller, AppImagePathNew);
         return ValueTask.CompletedTask;
     }
 
     public async ValueTask InstallUpdateAsync()
     {
-        if (!File.Exists(InstallerPath))
+        // TODO implement linux installer
+        if (!File.Exists(AppImagePathNew))
         {
-            throw new FileNotFoundException("Installer not found", InstallerPath);
+            throw new FileNotFoundException("Installer not found", AppImagePathNew);
         }
 
-        var vrcxProcess = new Process
+        var runningAppImagePath = Process.GetCurrentProcess().MainModule?.FileName;
+        if (runningAppImagePath == null || !File.Exists(runningAppImagePath))
+        {
+            throw new FileNotFoundException("Current VRCX executable file path not found", runningAppImagePath);
+        }
+
+        if (File.Exists(AppImagePathOld))
+            File.Delete(AppImagePathOld);
+        File.Move(runningAppImagePath, AppImagePathOld);
+        File.Move(AppImagePathNew, runningAppImagePath);
+        var process = new Process
         {
             StartInfo = new ProcessStartInfo
             {
-                FileName = InstallerPath,
-                UseShellExecute = true,
-                WorkingDirectory = AppPathService.AppDataDirectory
+                FileName = "chmod",
+                Arguments = $"+x {runningAppImagePath}"
             }
         };
-
-        vrcxProcess.Start();
+        process.Start();
+        await process.WaitForExitAsync();
 
         await platformLifetimeService.InvokeShutdownAsync();
     }
 
     public ValueTask CleanupAfterInstallationAsync()
     {
-        if (File.Exists(InstallerPath))
+        if (File.Exists(AppImagePathNew))
         {
-            _logger.Information("Cleaning up installer at {InstallerPath}", InstallerPath);
-            File.Delete(InstallerPath);
+            _logger.Information("Cleaning up installer at {AppImagePathNew}", AppImagePathNew);
+            File.Delete(AppImagePathNew);
         }
 
         return ValueTask.CompletedTask;
@@ -65,10 +77,11 @@ public sealed class LinuxUpdateInstallationService(
 
     public ValueTask CancelUpdateInstallationAsync()
     {
-        if (File.Exists(InstallerPath))
+        if (File.Exists(AppImagePathNew))
         {
-            _logger.Information("Cancelling update installation and deleting installer at {InstallerPath}", InstallerPath);
-            File.Delete(InstallerPath);
+            _logger.Information("Cancelling update installation and deleting installer at {AppImagePathNew}",
+                AppImagePathNew);
+            File.Delete(AppImagePathNew);
         }
 
         return ValueTask.CompletedTask;
